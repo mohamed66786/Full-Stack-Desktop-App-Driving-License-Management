f22332fad010302b27435365d9b5061703c2d6e8
mohamed667866 <mohamedtarek70900@gmail.com>
2024-09-25 15:03:12 +0300

    Creat the connection with database using ADO.NET

---
+++ DVLD_DataAccess/clsDataAccessSettings.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace DVLD_DataAccess
+{
+    static class clsDataAccessSettings
+    {
+        // Replace with your actual connection string
+        public static string ConnectionString = "Server=localhost\\MSSQLSERVER1;Database=DVLD;User Id=sa;Password=123456;"; 
+    }
+}
---
+++ DVLD_DataAccess/clsPersonData.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DVLD_DataAccess
+{
+    public class clsPersonData
+    {
+        public static bool GetPersonInfoByID(int PersonID, ref string FirstName, ref string SecondName,
+   ref string ThirdName, ref string LastName, ref string NationalNo, ref DateTime DateOfBirth,
+    ref short Gendor, ref string Address, ref string Phone, ref string Email,
+    ref int NationalityCountryID, ref string ImagePath)
+        {
+            bool isFound = false;
+            // creating connection with database using the connectionString using ADO.NET
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = "SELECT * FROM People WHERE PersonID = @PersonID";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            // this built in function to pass the parameter from query to the existing variable or parameter
+            // 
+            command.Parameters.AddWithValue("@PersonID", PersonID);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    // The record was found
+                    isFound = true;
+
+                    FirstName = (string)reader["FirstName"];
+                    SecondName = (string)reader["SecondName"];
+
+                    //ThirdName: allows null in database so we should handle null
+                    if (reader["ThirdName"] != DBNull.Value)
+                    {
+                        ThirdName = (string)reader["ThirdName"];
+                    }
+                    else
+                    {
+                        ThirdName = "";
+                    }
+
+                    LastName = (string)reader["LastName"];
+                    NationalNo = (string)reader["NationalNo"];
+                    DateOfBirth = (DateTime)reader["DateOfBirth"];
+                    Gendor = (byte)reader["Gendor"];
+                    Address = (string)reader["Address"];
+                    Phone = (string)reader["Phone"];
+
+
+                    //Email: allows null in database so we should handle null
+                    if (reader["Email"] != DBNull.Value)
+                    {
+                        Email = (string)reader["Email"];
+                    }
+                    else
+                    {
+                        Email = "";
+                    }
+
+                    NationalityCountryID = (int)reader["NationalityCountryID"];
+
+                    //ImagePath: allows null in database so we should handle null
+                    if (reader["ImagePath"] != DBNull.Value)
+                    {
+                        ImagePath = (string)reader["ImagePath"];
+                    }
+                    else
+                    {
+                        ImagePath = "";
+                    }
+
+                }
+                else
+                {
+                    // The record was not found
+                    isFound = false;
+                }
+
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+
+                isFound = false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return isFound;
+        }
+    }
+
+
+
+
+
+}
